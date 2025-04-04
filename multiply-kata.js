378c6a5923f77f298091a8d6f9c06e1d4eb6eae0
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-04-04 08:19:28 -0400

    Update multiply-kata.js

--- multiply-kata.js
+++ multiply-kata.js
@@ -1,8 +1,8 @@
 /* Multiplication */
 
-// Create 2 variables, a & b, and set to ne random integers between 3 and 9
+// Create 2 variables, a & b, and set to random integers between 3 and 9
 // Set a variable called product to be the product of a and b
-// Set a variable called equation to display  a * b = ??
+// Set a variable called equation to display: a * b = ??
 // Ask the equation of the user and store the user input in a variable called answer
 // Say "Correct!" if the answer and product match, "Incorrect!" if not.
 
